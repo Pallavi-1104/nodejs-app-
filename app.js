902b027f3238abe841e7d1b9879e4d5179714887
Pallavi-1104 <tarkeshubham2006@gmail.com>
2025-05-08 15:31:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js App deployed via GitLab CI/CD!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
